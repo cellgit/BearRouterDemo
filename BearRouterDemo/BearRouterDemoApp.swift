f04cf8569c1177572c31f6ccd03f42f1ea2ce1bf
liuhongli <liuhongli2120@gmail.com>
2025-01-24 14:08:31 +0800

    [t0]init bear router demo

---
+++ BearRouterDemo/BearRouterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearRouterDemoApp.swift
+//  BearRouterDemo
+//
+//  Created by admin on 2025/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BearRouterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BearRouterDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BearRouterDemo
+//
+//  Created by admin on 2025/1/24.
+//
+
+import SwiftUI
+import BearRouter
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
