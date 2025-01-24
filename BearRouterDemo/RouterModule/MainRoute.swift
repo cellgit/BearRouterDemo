858a5f1ee55dd80713b682a85495727c088d1a11
liuhongli <liuhongli2120@gmail.com>
2025-01-24 15:07:41 +0800

    [t0]路由使用示例2

---
+++ BearRouterDemo/RouterModule/MainRoute.swift
@@ -0,0 +1,37 @@
+//
+//  MainRoute.swift
+//  BearRouterDemo
+//
+//  Created by admin on 2025/1/24.
+//
+
+import SwiftUI
+import BearRouter
+
+/// 主模块的路由
+enum MainRoute: RouteProtocol {
+    case home
+    case detail(message: String)
+
+    var id: String {
+        switch self {
+        case .home:
+            return "main_home"
+        case let .detail(msg):
+            return "main_detail_\(msg)"
+        }
+    }
+}
+
+
+// 路由到视图的映射扩展
+extension MainRoute {
+    func destinationView() -> AnyView {
+        switch self {
+        case .home:
+            return AnyView(HomeView())
+        case let .detail(msg):
+            return AnyView(DetailView(message: msg))
+        }
+    }
+}
